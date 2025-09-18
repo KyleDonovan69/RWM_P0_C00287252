7285404165bd50fa8af563e995e59955d0640883
kyledonovan69 <c00287252@setu.ie>
2025-09-18 13:39:18 +0100

    cloning

--- src/lib/filters/combined/combined.ts
+++ src/lib/filters/combined/combined.ts
@@ -1,12 +1,12 @@
 // src/lib/combined/combined.ts
 
-import { collatzs as myCollatz } from '$lib/filters/collatz/collatz';
+import { collatzs as myCollatz } from '$lib/filters/collatz';
 
 // Prefer importing from the peer package root (they should export from index.ts)
 
-import { persistance as peerPersistance } from 'peer-filter-c00285423/src/lib/filters';
+import { persistence as peerPersistence } from 'peer-filter-c00285423/src/lib/filters';
 
 export function combinedFilter(xs: number[]): number[] {
     const afterMine = myCollatz(xs);
-    return peerPersistance(afterMine);
+    return peerPersistence(afterMine);
 }
